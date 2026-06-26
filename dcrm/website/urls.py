0a73c81121643e41465e97a148ec4d4ec94d9847
jhon jader <jaderperna@gmail.com>
2026-06-26 01:15:53 -0500

    Bueno aqui cree el archivo de los templates del diseño add_order y el de order con la seguridad de csrf token que fue la que elegi para que los datos esten encriptado sy mejor protegidos aparte de eso y tambien defini las rutas directamente en url para que todo quedara correcto!

--- dcrm/website/urls.py
+++ dcrm/website/urls.py
@@ -2,12 +2,15 @@ from django.urls import path
 from . import views  # Importa el archivo views.py de la misma carpeta
 
 urlpatterns = [  # type: ignore
-    path('', views.home, name='home'), # Aquí SÍ va el path # type: ignore
-    path('login/', views.login_user, name='login'), # type: ignore
-    path('logout/', views.logout_user, name='logout'), # type: ignore
-    path('registrar/',views.register_user, name='register'),
-    path('record/<str:pk>', views.customer_record, name='customer_record'), # type: ignore
-    path('delete-record/<str:pk>', views.delete_record, name='delete_record'), # type: ignore
-    path('update-record/<str:pk>', views.update_record, name='update_record'), # type: ignore
-
+    path("", views.home, name="home"),  # Aquí SÍ va el path # type: ignore
+    path("login/", views.login_user, name="login"),  # type: ignore
+    path("logout/", views.logout_user, name="logout"),  # type: ignore
+    path("registrar/", views.register_user, name="register"),
+    path("record/<str:pk>", views.customer_record, name="customer_record"),  # type: ignore
+    path("delete-record/<str:pk>", views.delete_record, name="delete_record"),  # type: ignore
+    path("update-record/<str:pk>", views.update_record, name="update_record"),  # type: ignore
+    # Ruta para ver el listado general de pedidos (con paginación segura)
+    path("pedidos/", views.list_orders, name="list_orders"),
+    # Ruta para acceder al formulario y procesar la creación de un nuevo pedido
+    path("crear-pedido/", views.create_order, name="create_order"),
 ]
